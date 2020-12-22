80d8a04db3731033e2c887839701a83cc599fec1
mumu <tnddls2ek@naver.com>
2020-12-22 20:26:19 +0900

    기본 naverMap 화면에 띄우기

--- NaverMapDemoTest/ViewController.swift
+++ NaverMapDemoTest/ViewController.swift
@@ -6,15 +6,28 @@
 //
 
 import UIKit
+import NMapsMap
+
+public let DEFAULT_CAMERA_POSITION = NMFCameraPosition(NMGLatLng(lat: 37.5666102, lng: 126.9783881), zoom: 15, tilt: 0, heading: 0)
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var naverMapView: NMFNaverMapView!
+    
+    var mapView : NMFMapView {
+        return naverMapView.mapView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        //첫번째 Test
+        
+        mapView.moveCamera(NMFCameraUpdate(position: DEFAULT_CAMERA_POSITION))
+        
     }
-
-
+}
+extension NMGLatLng {
+    func positionString() -> String {
+        return String(format: "(%.5f, %.5f)", lat, lng)
+    }
 }
 
